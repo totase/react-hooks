beb7bfe88341425265fdada408ae8af08ec34e41
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2024-03-28 20:36:02 +0100

    :wrench: Setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    pool: 'forks',
+    coverage: {
+      provider: 'v8',
+      reporter: ['html'],
+      reportsDirectory: './coverage/',
+    },
+  },
 });
