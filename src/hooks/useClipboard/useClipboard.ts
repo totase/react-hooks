dd3d7be51ce5a86f80d1dc7aba6586cda0076c3f
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2024-04-28 13:47:01 +0200

    :white_check_mark: Fix linter

--- src/hooks/useClipboard/useClipboard.ts
+++ src/hooks/useClipboard/useClipboard.ts
@@ -25,7 +25,6 @@ const useClipboard = () => {
         console.warn('Failed to copy', error);
         setCopiedText(null);
       });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return {
