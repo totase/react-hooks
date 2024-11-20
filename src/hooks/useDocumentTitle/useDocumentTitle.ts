09638017ac824d64f877e0b533a78aaa3b0b269b
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2024-11-20 21:05:28 +0100

    ✨ useDocumentTitle hook (#152)
    * 📦 npm audit
    
    * ✨ Add useDocumentTitle
    
    * 🎨 Add test and readme for useDocumentTitle

---
+++ src/hooks/useDocumentTitle/useDocumentTitle.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+export const APPLICATION_NAME = 'Hooks';
+
+/**
+ * Dynamically sets the document title. Title is reset when hook is unmounted.
+ *
+ * @param title - The title to set.
+ * @param appendName - Whether to append the application name to the title.
+ *
+ * @returns void
+ */
+const useDocumentTitle = (title: string, appendName = true) => {
+  useEffect(() => {
+    if (appendName) document.title = `${title} | ${APPLICATION_NAME}`;
+    else document.title = title;
+
+    return () => {
+      document.title = APPLICATION_NAME;
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [title]);
+};
+
+export default useDocumentTitle;
